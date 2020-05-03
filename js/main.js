6f040a6b7f5adee5811a395b58ce254502fade41
kentng01 <57498837+kentng01@users.noreply.github.com>
2020-05-03 14:34:26 -0400

    I'm bored.

---
+++ js/main.js
@@ -0,0 +1,72 @@
+hh1const choices = document.querySelectorAll('.choice');
+const score = document.getElementById('score');
+const result = document.getElementById('result');
+const restart = document.getElementById('restart');
+const modal = document.querySelector('.modal');
+const scoreboard = {
+    player: 0,
+    computer: 0
+}
+
+// Play game
+function play(e){
+    restart.style.display = 'inline-block';
+    const playerChoice = e.target.id;
+    const computerChoice = getComputerChoice();
+    const winner = getWinner(playerChoice, computerChoice);
+
+    console.log(playerChoice, computerChoice, winner)
+}
+
+// Get computers choice
+function getComputerChoice(){
+    const rand = Math.random();
+    if(rand < 0.34) {
+        return 'rock';
+    } else if(rand <= 0.67) {
+        return 'paper';
+    } else {
+        return 'scissors';
+    }
+}
+
+// Get game winner
+function getWinner(p, c){
+    if(p === c){
+        return 'draw';
+    } else if(p === 'rock'){
+        if(c === 'paper') {
+            return 'computer';
+        } else{
+            return 'player';
+        }
+    } else if(p === 'paper') {
+        if(c === 'scissors'){
+            return 'computer';
+        } else {
+            return 'player';
+        }
+    } else if(p === 'scissors'){
+        if(c === 'rock'){
+            return 'computer';
+        } else {
+            return 'player';
+        }
+    }
+}
+
+function showWinner(winner, computerChoice){
+    if(winner === 'player'){
+        // Inc player score
+        scoreboard.player++;
+        // Show modal result
+        result.innerHTML = `
+            <h1 class=“text-win">You Win</h1>
+            <i class="fas fa-hand-${computerChoice} fa-10x"></i>
+            <p>Computer chose <strong>${computerChoice}</strong></p>
+        `;
+    } else if
+}
+
+// Event listeners
+choices.forEach(choice => choice.addEventListener('click', play));
