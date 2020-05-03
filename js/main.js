a28d52daa649a8e4b9af06a74a858d74dbaf4e19
kentng01 <57498837+kentng01@users.noreply.github.com>
2020-05-03 14:47:08 -0400

    I FINISHED FINALLY!

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-hh1const choices = document.querySelectorAll('.choice');
+const choices = document.querySelectorAll('.choice');
 const score = document.getElementById('score');
 const result = document.getElementById('result');
 const restart = document.getElementById('restart');
@@ -14,8 +14,8 @@ function play(e){
     const playerChoice = e.target.id;
     const computerChoice = getComputerChoice();
     const winner = getWinner(playerChoice, computerChoice);
+    showWinner(winner, computerChoice);
 
-    console.log(playerChoice, computerChoice, winner)
 }
 
 // Get computers choice
@@ -65,8 +65,49 @@ function showWinner(winner, computerChoice){
             <i class="fas fa-hand-${computerChoice} fa-10x"></i>
             <p>Computer chose <strong>${computerChoice}</strong></p>
         `;
-    } else if
+    } else if(winner === 'computer'){
+                // Inc computer score
+                scoreboard.computer++;
+                // Show modal result
+                result.innerHTML = `
+                    <h1 class=“text-lose">You Lose</h1>
+                    <i class="fas fa-hand-${computerChoice} fa-10x"></i>
+                    <p>Computer chose <strong>${computerChoice}</strong></p>
+                `;
+    } else {
+        result.innerHTML = `
+                    <h1>It's a Draw!</h1>
+                    <i class="fas fa-hand-${computerChoice} fa-10x"></i>
+                    <p>Computer chose <strong>${computerChoice}</strong></p>
+                `;
+    }
+    // Show score
+    score.innerHTML = `
+    <p>Player: ${scoreboard.player}</p>
+    <p>Computer: ${scoreboard.computer}</p>
+    `;
+
+modal.style.display = 'block';
+}
+
+// Restart Game
+function restartGame(){
+    scoreboard.player = 0;
+    scoreboard.computer = 0;
+    score.innerHTML = `
+    <p>Player: 0</p>
+    <p>Computer: 0</p>
+    `;
+}
+
+// Clear modal
+function clearModal(e){
+    if(e.target == modal){
+        modal.style.display = 'none';
+    }
 }
 
 // Event listeners
 choices.forEach(choice => choice.addEventListener('click', play));
+window.addEventListener('click', clearModal);
+restart.addEventListener('click', restartGame);
